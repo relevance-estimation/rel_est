0040a6ecdeb3bc677b5250fbc037a21f119e343c
Roma <zotov.roman.2001@gmail.com>
2021-06-15 02:29:12 +0400

    uploading version 1.2.3

--- relevant_video.py
+++ relevant_video.py
@@ -19,18 +19,18 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.page = RelevantMoment(self)
+        self.page = RelevantVideo(self)
         self.setCentralWidget(self.page)
 
         self.show()
 
-class RelevantMoment(QWidget):
+class RelevantVideo(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("Редактирование рекламы")
+        self.setWindowTitle("Релевантное видео")
         self.adEditBlock = QWidget(self)
         self.adEditBlock.setGeometry(0, 0, 500, 500)
         self.adEditBlock.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
@@ -38,15 +38,15 @@ class RelevantMoment(QWidget):
         #self.adEditBlock.setMaximumSize(500, 250)
 
 
-        self.namewindow=QLabel('<h1>Релевантные моменты</h1>')
+        self.namewindow=QLabel('<h1>Релевантное видео</h1>')
         self.namewindow.setAlignment(Qt.AlignCenter)
 
         #self.namewindow.setFixedHeight()
 
-        self.nameInfoVid=QLabel('Инфо-видео')
-        self.nameInfoRec=QLabel('Инфо-реклама')
-        self.nameVid=QLabel('Видео')
-        self.nameRec=QLabel('Реклама')
+        self.nameInfoVid=QLabel('Набор видео')
+        self.nameInfoRec=QLabel('Набор реклам')
+        self.nameVid=QLabel('Набор видео')
+        self.nameRec=QLabel('Набор реклам')
 
         self.buttonInfoVid = QPushButton('Обзор', self)
         self.buttonInfoRec = QPushButton('Обзор', self)
@@ -116,68 +116,21 @@ class RelevantMoment(QWidget):
     def buttonWin1_onClick(self):
         select_file = getOpenFilesAndDirs()
         if select_file:
-            self.pathInfoVid.clear()
-            self.pathInfoVid.insert(select_file[0])
-            if (self.pathInfoVid.displayText()!=""
-                    and self.pathInfoRec.displayText()!=""
-                    and self.pathVid.displayText()!=""
-                    and self.pathRec.displayText()!=""):
-                self.buttonEdit.setEnabled(True)
-        else:
-            msg = QtWidgets.QMessageBox.information(self, 'Message', 'Вы ничего не выбрали.')
-
-    def buttonWin2_onClick(self):
-        select_file = getOpenFilesAndDirs()
-        if select_file:
-            self.pathInfoRec.clear()
-            self.pathInfoRec.insert(select_file[0])
-            if (self.pathInfoVid.displayText()!=""
-                    and self.pathInfoRec.displayText()!=""
-                    and self.pathVid.displayText()!=""
-                    and self.pathRec.displayText()!=""):
-                self.buttonEdit.setEnabled(True)
-        else:
-            msg = QtWidgets.QMessageBox.information(self, 'Message', 'Вы ничего не выбрали.')
-
-    def buttonWin3_onClick(self):
-        select_file = getOpenFilesAndDirs()
-        if select_file:
-            self.pathVid.clear()
-            self.pathVid.insert(select_file[0])
-            if (self.pathInfoVid.displayText()!=""
-                    and self.pathInfoRec.displayText()!=""
-                    and self.pathVid.displayText()!=""
-                    and self.pathRec.displayText()!=""):
-                self.buttonEdit.setEnabled(True)
-        else:
-            msg = QtWidgets.QMessageBox.information(self, 'Message', 'Вы ничего не выбрали.')
-
-    def buttonWin4_onClick(self):
-        select_file = getOpenFilesAndDirs()
-        if select_file:
-            self.pathRec.clear()
-            self.pathRec.insert(select_file[0])
-            if (self.pathInfoVid.displayText() != ""
-                    and self.pathInfoRec.displayText() != ""
-                    and self.pathVid.displayText() != ""
-                    and self.pathRec.displayText() != ""):
-                self.buttonEdit.setEnabled(True)
+            self.pathEdit.clear()
+            self.pathEdit.insert(select_file[0])
+            self.buttonEdit.setEnabled(True)
         else:
             msg = QtWidgets.QMessageBox.information(self, 'Message', 'Вы ничего не выбрали.')
 
 
 
-    def center(self):
 
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
 
 
 def getOpenFilesAndDirs(parent=None, caption='', directory='',
                         filter='', initialFilter='', options=None):
     def updateText():
+        # обновить содержимое виджета редактирования строки выбранными файлами
         selected = []
         for index in view.selectionModel().selectedRows():
             selected.append('"{}"'.format(index.data()))
@@ -195,13 +148,25 @@ def getOpenFilesAndDirs(parent=None, caption='', directory='',
         if initialFilter:
             dialog.selectNameFilter(initialFilter)
 
+    # по умолчанию, если каталог открыт в режиме списка файлов,
+    # QFileDialog.accept() показывает содержимое этого каталога,
+    # но нам нужно иметь возможность "открывать" и каталоги, как мы можем делать с файлами,
+    # поэтому мы просто переопределяем `accept()` с реализацией QDialog по умолчанию,
+    # которая просто вернет `dialog.selectedFiles()`
+
     dialog.accept = lambda: QtWidgets.QDialog.accept(dialog)
+
+    # в неродном диалоге есть много представлений элементов,
+    # но те, которые отображают фактическое содержимое, создаются внутри QStackedWidget;
+    # это QTreeView и QListView, и дерево используется только тогда,
+    # когда viewMode установлен на QFileDialog.Details, что не в этом случае.
+
     stackedWidget = dialog.findChild(QtWidgets.QStackedWidget)
     view = stackedWidget.findChild(QtWidgets.QListView)
     view.selectionModel().selectionChanged.connect(updateText)
 
     pathEdit = dialog.findChild(QtWidgets.QLineEdit)
-
+    # очищаем содержимое строки редактирования всякий раз, когда изменяется текущий каталог
     dialog.directoryEntered.connect(lambda: pathEdit.setText(''))
 
     dialog.exec_()

74412e83f6d7a968d40a8b0a4cf502052191cbad
RaisHab <34600995+RaisHibi@users.noreply.github.com>
2021-06-15 05:00:51 +0400

    Remove signals from page

--- ad_edit_page_1.py
+++ ad_edit_page_1.py
@@ -46,9 +46,9 @@ class AdEditPage1(QWidget):
         self.buttonEdit.setMaximumWidth(100)
         self.buttonoverview.setMaximumWidth(100)
 
-        self.buttonEdit.setEnabled(False)
+        #self.buttonEdit.setEnabled(False)
 
-        self.buttonoverview.clicked.connect(self.buttonWin1_onClick)
+        #self.buttonoverview.clicked.connect(self.buttonWin1_onClick)
 
         grid = QGridLayout()
         grid.setSpacing(10)
@@ -73,79 +73,6 @@ class AdEditPage1(QWidget):
         self.pageVbox.addLayout(self.pageHbox)
 
         self.setLayout(self.pageVbox)
-
-    def buttonWin1_onClick(self):
-        select_file = getOpenFilesAndDirs()
-        if select_file:
-            self.pathEdit.clear()
-            self.pathEdit.insert(select_file[0])
-            self.buttonEdit.setEnabled(True)
-        else:
-            msg = QtWidgets.QMessageBox.information(self, 'Message', 'Вы ничего не выбрали.')
-
-    def buttonWin2_onClick(self):
-        QMessageBox.information(None, 'Сообщение от программы', "Робит")
-        signal_state = False
-
-    def switch(self, signal_state):
-        if signal_state:
-            signal_state=False
-        else:
-            signal_state=True
-
-    def center(self):
-
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-
-def getOpenFilesAndDirs(parent=None, caption='', directory='',
-                        filter='', initialFilter='', options=None):
-    def updateText():
-        # обновить содержимое виджета редактирования строки выбранными файлами
-        selected = []
-        for index in view.selectionModel().selectedRows():
-            selected.append('"{}"'.format(index.data()))
-        pathEdit.setText(' '.join(selected))
-
-    dialog = QtWidgets.QFileDialog(parent, windowTitle=caption)
-    dialog.setFileMode(dialog.ExistingFiles)
-    if options:
-        dialog.setOptions(options)
-    dialog.setOption(dialog.DontUseNativeDialog, True)  # !!!
-    if directory:
-        dialog.setDirectory(directory)
-    if filter:
-        dialog.setNameFilter(filter)
-        if initialFilter:
-            dialog.selectNameFilter(initialFilter)
-
-    # по умолчанию, если каталог открыт в режиме списка файлов,
-    # QFileDialog.accept() показывает содержимое этого каталога,
-    # но нам нужно иметь возможность "открывать" и каталоги, как мы можем делать с файлами,
-    # поэтому мы просто переопределяем `accept()` с реализацией QDialog по умолчанию,
-    # которая просто вернет `dialog.selectedFiles()`
-
-    dialog.accept = lambda: QtWidgets.QDialog.accept(dialog)
-
-    # в неродном диалоге есть много представлений элементов,
-    # но те, которые отображают фактическое содержимое, создаются внутри QStackedWidget;
-    # это QTreeView и QListView, и дерево используется только тогда,
-    # когда viewMode установлен на QFileDialog.Details, что не в этом случае.
-
-    stackedWidget = dialog.findChild(QtWidgets.QStackedWidget)
-    view = stackedWidget.findChild(QtWidgets.QListView)
-    view.selectionModel().selectionChanged.connect(updateText)
-
-    pathEdit = dialog.findChild(QtWidgets.QLineEdit)
-    # очищаем содержимое строки редактирования всякий раз, когда изменяется текущий каталог
-    dialog.directoryEntered.connect(lambda: pathEdit.setText(''))
-
-    dialog.exec_()
-    return dialog.selectedFiles()
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

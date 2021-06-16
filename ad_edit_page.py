f077b3d1861d5b74f090f0ead41340a51cea951f
Roma <zotov.roman.2001@gmail.com>
2021-06-16 23:06:42 +0400

    adEditPage controller

--- ad_edit_page.py
+++ ad_edit_page.py
@@ -3,11 +3,12 @@
 
 import sys
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout,
-                            QPushButton, QLabel, QHBoxLayout, QSizePolicy, QLineEdit, QFileDialog)
+                            QPushButton,QMessageBox, QLabel, QHBoxLayout, QSizePolicy, QLineEdit, QFileDialog)
 from PyQt5.QtCore import QRect, Qt
 from PyQt5.QtGui import QFont
 from ad_edit_page_1 import AdEditPage1
 from ad_edit_page_2 import AdEditPage2
+import pickle
 
 
 class App(QMainWindow):
@@ -22,6 +23,9 @@ class App(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         self.page = AdEditPage(self)
+
+        self.page_controller = AdEditPageConntroller(self.page)
+
         self.setCentralWidget(self.page)
 
         self.show()
@@ -61,12 +65,11 @@ class AdEditPage(QWidget):
         self.tab1.buttonEdit.clicked.connect(self.launch_success)
 
     def browse_file(self):
-        select_file = QFileDialog.getOpenFileName(self)
-        if select_file[0] != "":
-            self.tab1.pathEdit.clear()
-            self.tab1.pathEdit.insert(select_file[0])
-            self.tab2.chosenPathLabel.setText(select_file[0])
-            self.tab1.buttonEdit.setEnabled(True)
+            select_file = QFileDialog.getOpenFileName(self)
+            if select_file[0] != "":
+                self.tab1.pathEdit.clear()
+                self.tab1.pathEdit.insert(select_file[0])
+                self.tab1.buttonEdit.setEnabled(True)
 
     def launch_success(self):
         self.tabs.setTabEnabled(1, True)
@@ -81,6 +84,39 @@ class AdEditPage(QWidget):
 
     def put_keywords(self, keywords):
         self.tab2.keywordsEditLine.setText(keywords)
+
+    def show_error(self, error):
+        QMessageBox.critical(self,"Ошибка", error)
+
+    def launch_fail(self):
+        self.tab1.buttonEdit.setEnabled(False)
+
+class AdEditPageConntroller():
+    def __init__(self, ad_edit_page):
+        self.ad_edit_page = ad_edit_page
+        self.signals()
+
+    def signals(self):
+        self.ad_edit_page.edit_slot(self.check_file())
+
+    def check_file(self):
+        try:
+            filename = self.ad_edit_page.get_file_path()
+            with open(filename, "rb") as f:
+                vids = pickle.load(f)
+        except:
+            self.ad_edit_page.show_error("Ошибка при чтении файла")
+            self.ad_edit_page.launch_fail()
+            return
+        try:
+            keywords="".join(vids.videos[0].keywords)
+            self.ad_edit_page.put_keywords(keywords)
+            self.ad_edit_page.launch_success()
+        except:
+            self.ad_edit_page.show_error( "Ошибка доступа к ключевым словам")
+            self.ad_edit_page.launch_fail()
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
